32c0027bfb955664a3260fc573e5efd717356dd2
abdul2112 <abdulkadir.s.salad1@gmail.com>
2021-04-27 14:25:00 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNavBar from './components/MyNavBar';
+
+function App() {
+  return (
+    <div>
+      <MyNavBar />
+    </div>
+  );
+}
+
+export default App;
